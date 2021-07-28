0f42e31473fc319f8cbe1e28323435887b7dcfaa
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-07-28 21:16:14 +0300

    lab1/Galanin_Pavel : Updated task4

--- lab1/src/task4/task4/MainWindow.xaml.cs
+++ lab1/src/task4/task4/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace task4
         {
             InitializeComponent();
         }
+
+        private void EraseByPoint(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void EraseByStroke(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void GestureOnly(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.GestureOnly;
+        }
+
+        private void Ink(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void InkAndGesture(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.InkAndGesture;
+        }
+
+        private void None(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.None;
+        }
+
+        private void Select(object sender, RoutedEventArgs e)
+        {
+            ink.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void BrushWidthChange(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ink.DefaultDrawingAttributes.Width = ChooseWidth.Value;
+            ink.DefaultDrawingAttributes.Height = ChooseWidth.Value;
+        }
+
+        private void ColorsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selectedItem = (ComboBoxItem)(ColorsList.SelectedValue);
+            string value = (string)(selectedItem.Content);
+            ink.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString(value);
+        }
     }
 }
