7a951cff7d0083b4d404a178967b8b3ac41f3249
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-07-28 22:06:17 +0300

    lab4/Galanin_Pavel : Updated task1

--- lab4/src/task1/task1/MainWindow.xaml.cs
+++ lab4/src/task1/task1/MainWindow.xaml.cs
@@ -24,5 +24,56 @@ namespace task1
         {
             InitializeComponent();
         }
+
+        private void button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            int ButtonWidth = (int)button.Width;
+            int ButtonHeight = (int)button.Height;
+            int MaxWidth = 800;
+            int MaxHeight = 450;
+            int xButtonCord = (int)Canvas.GetLeft(button);
+            int yButtonCord = (int)Canvas.GetTop(button);
+
+            int xNewCord = (int)xButtonCord;
+            int yNewCord = (int)yButtonCord;
+
+            int Nx = 0;
+            do
+            {
+                xNewCord = xButtonCord + rnd.Next(-ButtonWidth, ButtonWidth * 2);
+                Nx += 1;
+                if (Nx >= 100)
+                {
+                    MessageBox.Show("Зациклен цикл");
+                    break;
+                }
+            }
+            while (xNewCord <= 0 || (xNewCord + ButtonWidth) >= MaxWidth);
+
+            int Ny = 0;
+            do
+            {
+                yNewCord = yButtonCord + rnd.Next(-ButtonHeight, ButtonHeight * 2);
+                Ny += 1;
+                if (Ny >= 100)
+                {
+                    MessageBox.Show("Зациклен цикл");
+                    break;
+                }
+            }
+            while (yNewCord <= 0 || (yNewCord + ButtonHeight) >= MaxHeight);
+
+            Canvas.SetLeft(button, xNewCord);
+            Canvas.SetTop(button, yNewCord);
+
+            MyTextBox.Text = ""
+                + "MaxWidth = " + MaxWidth.ToString() + "\n"
+                + "MaxHeight = " + MaxHeight.ToString() + "\n"
+                + "xButtonCord = " + xButtonCord.ToString() + "\n"
+                + "yButtonCord = " + yButtonCord.ToString() + "\n"
+                + "xNewCord = " + xNewCord.ToString() + "\n"
+                + "yNewCord = " + yNewCord.ToString() + "\n";
+        }
     }
 }
