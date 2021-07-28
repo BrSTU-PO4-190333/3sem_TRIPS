6d86e7bb310c8f7e5e7985cf5811574f3e486b52
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-07-28 20:49:54 +0300

    lab1/Galanin_Pavel : Updated task3

--- lab1/src/task3/task3/MainWindow.xaml.cs
+++ lab1/src/task3/task3/MainWindow.xaml.cs
@@ -24,5 +24,42 @@ namespace task3
         {
             InitializeComponent();
         }
+        public void ClearStatusBar(object sender, RoutedEventArgs e)
+        {
+            ApplicationStatusBar.Text = "...";
+        }
+        // = = = = = Change background color
+        private void ChangeBackgroundColor_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            ApplicationStatusBar.Text = "Click, if you want to change background color";
+        }
+
+        private void Button_GetBackgroundColor_setGridBackground(object sender, RoutedEventArgs e)
+        {
+            ApplicationGrid.Background = (sender as Button).Background;
+        }
+        private void MenuItem_GetBackgroundColor_setGridBackground(object sender, RoutedEventArgs e)
+        {
+            ApplicationGrid.Background = (sender as MenuItem).Background;
+        }
+
+        // = = = = = About the developer
+        private void AboutTheDev_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Developer: Pavel Galanin\n");
+        }
+        private void AboutTheDev_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            ApplicationStatusBar.Text = "Click, if you want to understand who is author";
+        }
+        // = = = = = Close application
+        private void CloseApplication_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void CloseApplication_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            ApplicationStatusBar.Text = "Click, if you want to close application";
+        }
     }
 }
